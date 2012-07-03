4df76827da15d36e2ba67bdb9b35c34f820695ca
Ilya Menshikov <unkmas@gmail.com>
2012-07-03 12:43:55 +0600

    fix

--- lib/big_sitemap/builder.rb
+++ lib/big_sitemap/builder.rb
@@ -10,8 +10,12 @@ class BigSitemap
       'xmlns:xsi' => "http://www.w3.org/2001/XMLSchema-instance",
       'xsi:schemaLocation' => "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
     }
+    
+    def self.lastmod
+      @@lastmod ||= {}
 
-    attr_reader :lastmod
+      @@lastmod
+    end
 
     def initialize(options)
       @@lastmod       ||= {}
